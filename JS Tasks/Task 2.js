f87b86cae042cbfe0d668307e9e7ae36cb50b49b
itz-Tanisha <122715158+itz-Tanisha@users.noreply.github.com>
2023-10-04 22:41:14 +0530

    Update Task 2.js

--- JS Tasks/Task 2.js
+++ JS Tasks/Task 2.js
@@ -1,5 +1,10 @@
-function alphabet_order(str)
-  {
-return str.split('').sort().join('');
-  }
-console.log(alphabet_order("webmaster"));
+      let str = document.getElementById("str");
+      function rev() {
+        let rev = "";
+        let n = str.value;
+        for (let i = n.length - 1; i >= 0; i--) {
+          rev += n[i];
+        }
+        document.getElementById("ans").innerHTML =
+          "The reverse of " + str.value + " is " + rev;
+      }
